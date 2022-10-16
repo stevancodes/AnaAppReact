db1df6cfa487c93ae378f36813ff8fd203b0104b
stevancodes <stevan.code@gmail.com>
2022-10-16 12:30:06 +0200

    Fixing bugs, added search on Enter key

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,43 +3,30 @@ import React, { Component } from "react";
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
   render() {
     return (
-      <>{!this.props.btnClicked &&
-        <div className="card" onClick={() => this.props.funkcija(this.props.movie2.id)}>
-          <img
-            src={
-              this.props.movie2.image.original
-            }
-            alt="slika"
-          ></img>
-          <div>
-            {this.props.movie2.name}
-          </div>
-          <p className="rating">
-            {this.props.movie2.rating.average ? this.props.movie2.rating.average : "Not defined"}
-          </p>
-        </div>}
-        {this.props.btnClicked &&
-          <div className="card" onClick={() => this.props.funkcija(this.props.movie.show.id)}>
-            <img
-              src={
-                this.props.movie.show.image.original
-              }
-              alt="slika"
-            ></img>
-            <div>
-              {this.props.movie.show.name}
-            </div>
+      <>
+        {!this.props.btnClicked && (
+          <div className="card" onClick={() => this.props.funkcija(this.props?.movie2?.id)}>
+            <img src={this.props?.movie2?.image?.original} alt="slika"></img>
+            <div>{this.props?.movie2?.name}</div>
             <p className="rating">
-              {this.props.movie.show.rating.average ? this.props.movie.show.rating.average : " Not defined"}
+              {this.props.movie2?.rating?.average ? this.props.movie2?.rating?.average : "Not defined"}
             </p>
-          </div>}
+          </div>
+        )}
+        {this.props.btnClicked && (
+          <div className="card" onClick={() => this.props.funkcija(this.props?.movie?.show?.id)}>
+            <img src={this.props?.movie?.show?.image?.original} alt="slika"></img>
+            <div>{this.props?.movie?.show?.name}</div>
+            <p className="rating">
+              {this.props?.movie?.show?.rating?.average ? this.props?.movie?.show?.rating?.average : " Not defined"}
+            </p>
+          </div>
+        )}
       </>
-
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,31 +14,56 @@ class Header extends Component {
     this.setState({ searchValue: e.target.value });
   }
 
-  handleFetch() {
-    this.props.handleFetchBtn.call(this)
-    this.setState({ searchValue: "" })
+  handleFetch(e) {
+    this.props.handleFetchBtn.call(this);
+    this.setState({ searchValue: "" });
+    if (e.key === "Enter") {
+      console.log("Hello");
+    }
   }
   handleInput(e) {
     this.props.handleQuery(e);
-    this.props.handleFetchRecomend.call(this)
+    this.props.handleFetchRecomend.call(this);
     this.setState({ searchValue: e.target.value }, () => console.log(this.state));
     // this.forceUpdate.call(this)
   }
   toggleClass() {
     const currentState = this.state.active;
     this.setState({ active: !currentState });
-  };
+  }
+
+  keyHandler(e) {
+    if (e.key === "Enter") {
+      console.log("Hello");
+      this.props.handleFetchBtn.call(this);
+      this.setState({ searchValue: "" });
+    }
+  }
 
   render() {
     return (
       <div id="header">
         <h2 onClick={this.props.funkcija}>BitShow</h2>
         <div className="inner-wrapper">
-          <input type="text" onInput={this.handleInput.bind(this)} value={this.state.searchValue} placeholder="Search shows..."></input>
-          <button onClick={this.handleFetch.bind(this)} className="search-button">Get Data</button>
-          <div className="containerRecomend">{this.props.movies &&
-            this.state.searchValue.length > 0 &&
-            this.props.movies.map((element, index) => <p key={index} onClick={() => this.props.funkcija2(element.show.id)}>{element.show.name}</p>)}</div>
+          <input
+            type="text"
+            onInput={this.handleInput.bind(this)}
+            value={this.state.searchValue}
+            onKeyUp={this.keyHandler.bind(this)}
+            placeholder="Search shows..."
+          ></input>
+          <button onClick={this.handleFetch.bind(this)} className="search-button">
+            Get Data
+          </button>
+          <div className="containerRecomend">
+            {this.props.movies &&
+              this.state.searchValue.length > 0 &&
+              this.props.movies.map((element, index) => (
+                <p key={index} onClick={() => this.props.funkcija2(element.show.id)}>
+                  {element.show.name}
+                </p>
+              ))}
+          </div>
         </div>
       </div>
     );
